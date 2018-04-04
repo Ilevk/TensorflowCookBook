02217af02285f21485823818bb4777e0fde3f11e
kcs93023 <kcs93023@gmail.com>
2018-04-04 21:24:44 +0900

    Add Iris_dataSet_Test.py Comment

--- Chap_02/7_Iris_dataSet_Test.py
+++ Chap_02/7_Iris_dataSet_Test.py
@@ -16,13 +16,16 @@ y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 A = tf.Variable(tf.random_normal(shape=[1, 1]))
 b = tf.Variable(tf.random_normal(shape=[1, 1]))
 
+#Define x1 - Ax2 + b = f
+#we can see which points above the line or under the line through f
 my_mult = tf.matmul(x2_data, A)
 my_add = tf.add(my_mult, b)
 my_output = tf.subtract(x1_data, my_add)
 
+#Define loss func
 xentropy = tf.nn.sigmoid_cross_entropy_with_logits(
     logits=my_output, labels=y_target)
-
+#Use GradientDescent Method
 my_opt = tf.train.GradientDescentOptimizer(0.05)
 train_step = my_opt.minimize(xentropy)
 
@@ -30,11 +33,16 @@ init = tf.global_variables_initializer();
 sess.run(init)
 
 for i in range(1000):
+    #pick random_Index
     rand_index = np.random.choice(len(iris_2d), size=batch_size)
+    #pick random x
     rand_x = iris_2d[rand_index]
+    #make ndarray x1,x2 use random x[0], x[1]
     rand_x1 = np.array([[x[0]] for x in rand_x])
     rand_x2 = np.array([[x[1]] for x in rand_x])
+    #make ndarray correct answer use same random index
     rand_y = np.array([[y] for y in binary_target[rand_index]])
+    #run train
     sess.run(train_step, feed_dict={x1_data: rand_x1, x2_data: rand_x2, y_target: rand_y})
 
     if (i + 1) % 200 == 0:
@@ -42,8 +50,9 @@ for i in range(1000):
 
 #print Graph
 [[slope]] = sess.run(A)
-[[intercept]T] = sess.run(b)
+[[intercept]] = sess.run(b)
 x = np.linspace(0, 3, num=50)
+#Make Line f
 ablineValues = []
 for i in x:
     ablineValues.append(slope * i + intercept)
@@ -52,6 +61,7 @@ setosa_y = [a[0] for i, a in enumerate(iris_2d) if binary_target[i] == 1]
 non_setosa_x = [a[1] for i, a in enumerate(iris_2d) if binary_target[i] == 0]
 non_setosa_y = [a[0] for i, a in enumerate(iris_2d) if binary_target[i] == 0]
 
+#Draw plot
 plt.plot(setosa_x, setosa_y, 'rx', ms=10, mew=2, label='setosa')
 plt.plot(non_setosa_x, non_setosa_y, 'ro', label='Non-setosa')
 plt.plot(x, ablineValues, 'b-')
