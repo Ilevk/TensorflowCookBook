af95e27dc980e75cf54ee34531325ecbf85f04f5
kcs93023 <kcs93023@gmail.com>
2018-04-10 18:32:18 +0900

    Edit comment Use_MultiLayer.py

--- Chap_06/05_Use_MultiLayer.py
+++ Chap_06/05_Use_MultiLayer.py
@@ -10,9 +10,11 @@ birth_file = requests.get(birthdata_url)
 birth_data = birth_file.text.split('\r\n')
 birth_header = birth_data[0].split('\t')
 
+#Define Values from birthday dataset
 birth_data = [[float(x) for x in y.split('\t') if len(x) >= 1] for y in birth_data[1:] if len(y) >= 1]
 y_vals = np.array([x[8] for x in birth_data])
 cols_of_interest = ['AGE', 'LWT', 'RACE', 'SMOKE', 'PTL', 'HT', 'UI']
+#Extract Data by columns of interest
 x_vals = np.array(
     [[x[ix] for ix, feature in enumerate(birth_header) if feature in cols_of_interest] for x in birth_data])
 
@@ -20,8 +22,9 @@ batch_size = 100
 seed = 3
 np.random.seed(seed)
 tf.set_random_seed(seed)
-
+#Get train indices
 train_indices = np.random.choice(len(x_vals), round(len(x_vals) * 0.8), replace=False)
+#Get Test indices (all - train indices)
 test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
 x_vals_train = x_vals[train_indices]
 x_vals_test = x_vals[test_indices]
@@ -34,7 +37,7 @@ def normalize_cols(m):
     col_min = m.min(axis=0)
     return (m - col_min) / (col_max - col_min)
 
-
+#Normalize data and Delete nan data
 x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
 x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
 
@@ -48,16 +51,16 @@ def init_bias(shape, st_dev):
     bias = tf.Variable(tf.random_normal(shape, stddev=st_dev))
     return bias
 
-
+#Define placehoder
 x_data = tf.placeholder(shape=[None, 7], dtype=tf.float32)
 y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
-
+#Define end of each layers
 def fully_connected(input_layer, weights, biases):
     layer = tf.add(tf.matmul(input_layer, weights), biases)
     return tf.nn.relu(layer)
 
-
+#Construct Network
 weight_1 = init_weight(shape=[7, 25], st_dev=10.0)
 bias_1 = init_bias(shape=[25], st_dev=10.0)
 layer_1 = fully_connected(x_data, weight_1, bias_1)
@@ -74,7 +77,9 @@ weight_4 = init_weight(shape=[3, 1], st_dev=10.0)
 bias_4 = init_bias(shape=[1], st_dev=10.0)
 final_output = fully_connected(layer_3, weight_4, bias_4)
 
+#Define loss func
 loss = tf.reduce_mean(tf.abs(y_target - final_output))
+#Use Adam method to minimize loss func
 my_opt = tf.train.AdamOptimizer(0.05)
 train_step = my_opt.minimize(loss)
 init = tf.global_variables_initializer()
@@ -82,6 +87,7 @@ sess.run(init)
 
 loss_vec = []
 test_loss = []
+#Train
 for i in range(200):
     rand_index = np.random.choice(len(x_vals_train), size=batch_size)
     rand_x = x_vals_train[rand_index]
@@ -95,7 +101,7 @@ for i in range(200):
 
     if (i + 1) % 25 == 0:
         print('Geration: ' + str(i + 1) + '. Loss = ' + str(temp_loss))
-
+#plotting
 plt.plot(loss_vec, 'k-', label='Train Loss')
 plt.plot(test_loss, 'r--', label='Test Loss')
 plt.title('Loss (MSE) per Generation')
@@ -111,6 +117,7 @@ test_preds = [x[0] for x in sess.run(final_output, feed_dict={x_data: x_vals_tes
 train_preds = [x[0] for x in sess.run(final_output, feed_dict={x_data: x_vals_train})]
 test_preds = np.array([1.0 if x < 2500.0 else 0.0 for x in test_preds])
 train_preds = np.array([1.0 if x < 2500.0 else 0.5 for x in train_preds])
+#Estimate Accuracy
 test_acc = np.mean([x==y for x,y, in zip(test_preds, test_actuals)])
 train_acc = np.mean([x==y for x,y in zip(train_preds, train_actuals)])
 print('On predicting the category of low birthweight from regression output (<2500g):')
