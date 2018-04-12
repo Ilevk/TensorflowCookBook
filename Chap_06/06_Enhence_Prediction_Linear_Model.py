5d30ca4a3be1fae4e4d71cae8d7bbc7cf6e42816
kcs93023 <kcs93023@gmail.com>
2018-04-12 20:27:04 +0900

    Add comment Enhence_Prediction_Linear_Model.py

--- Chap_06/06_Enhence_Prediction_Linear_Model.py
+++ Chap_06/06_Enhence_Prediction_Linear_Model.py
@@ -10,13 +10,17 @@ birth_file = requests.get(birthdata_url)
 birth_data = birth_file.text.split('\r\n')
 birth_header = birth_data[0].split('\t')
 
+#Define Values from birthday dataset
 birth_data = [[float(x) for x in y.split('\t') if len(x) >= 1] for y in birth_data[1:] if len(y) >= 1]
 birth_data = [[float(x) for x in row] for row in birth_data]
 
 y_vals = np.array([x[0] for x in birth_data])
 x_vals = np.array([x[2:9] for x in birth_data])
 
+#Get train indices
 train_indices = np.random.choice(len(x_vals), round(len(x_vals) * 0.8), replace=False)
+
+#Get Test indices (all - train indices)
 test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
 x_vals_train = x_vals[train_indices]
 x_vals_test = x_vals[test_indices]
@@ -29,7 +33,7 @@ def normalize_cols(m):
     col_min = m.min(axis=0)
     return (m - col_min) / (col_max - col_min)
 
-
+#Normalize data and Delete nan data
 x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
 x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
 
@@ -49,7 +53,7 @@ def logistic(input_layer, multiplication_weight, bias_weight, activation=True):
     else:
         return linear_layer
 
-
+#Construct Network
 A1 = init_variable(shape=[7, 14])
 b1 = init_variable(shape=[14])
 logistic_layer1 = logistic(x_data, A1, b1)
@@ -62,7 +66,9 @@ A3 = init_variable(shape=[5, 1])
 b3 = init_variable(shape=[1])
 final_output = logistic(logistic_layer2, A3, b3, activation=False)
 
+#Define loss func
 loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=final_output, labels=y_target))
+#Use Adam method to minimize loss func
 my_opt = tf.train.AdamOptimizer(learning_rate=0.002)
 train_step = my_opt.minimize(loss)
 init = tf.global_variables_initializer()
@@ -75,6 +81,7 @@ accuracy = tf.reduce_mean(predictions_correct)
 loss_vec = []
 train_acc = []
 test_acc = []
+#Train
 for i in range(1500):
     rand_index = np.random.choice(len(x_vals_train), size=batch_size)
     rand_x = x_vals_train[rand_index]
