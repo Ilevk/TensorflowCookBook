c8615462e873e3d6fe0bc828d02e3db234a3e84e
kcs93023 <kcs93023@gmail.com>
2018-04-13 21:49:09 +0900

    Add Chap_08 & 01_Simple_CNN_Test.py

--- Chap_01/MNIST_hand_writing_test.py
+++ Chap_01/MNIST_hand_writing_test.py
@@ -1,6 +1,7 @@
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print(len(mnist.train.images))
 print(len(mnist.test.images))
 print(len(mnist.validation.images))
-print(mnist.train.labels[1,:])
+print(mnist.train.labels[1, :])
