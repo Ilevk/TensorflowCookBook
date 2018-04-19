7c98dc601fefb253141201c9c249c9fba937ec22
kcs93023 <kcs93023@gmail.com>
2018-04-19 12:20:03 +0900

    Solving Problem in git

--- Chap_08/01_Simple_CNN_Test.py
+++ Chap_08/01_Simple_CNN_Test.py
@@ -5,13 +5,15 @@ from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
 sess = tf.Session()
 
+#Load Mnist DataSet
 data_dir = 'temp'
 mnist = read_data_sets(data_dir)
+#Make Train and Test Data
 train_xdata = np.array([np.reshape(x, (28, 28)) for x in mnist.train.images])
 test_xdata = np.array([np.reshape(x, (28, 28)) for x in mnist.test.images])
 train_labels = mnist.train.labels
 test_labels = mnist.test.labels
-
+#Define some of values
 batch_size = 100
 learning_rate = 0.005
 evaluation_size = 500
@@ -27,6 +29,7 @@ max_pool_size1 = 2
 max_pool_size2 = 2
 fully_connected_size1 = 100
 
+#Define Placeholder and Shape
 x_input_shape = (batch_size, image_width, image_height, num_channels)
 x_input = tf.placeholder(tf.float32, shape=x_input_shape)
 y_target = tf.placeholder(tf.int32, shape=batch_size)
@@ -34,6 +37,7 @@ eval_input_shape = (evaluation_size, image_width, image_height, num_channels)
 eval_input = tf.placeholder(tf.float32, shape=eval_input_shape)
 eval_target = tf.placeholder(tf.int32, shape=evaluation_size)
 
+#Define Each Conv layer's weight, bias
 conv1_weight = tf.Variable(tf.truncated_normal([4, 4, num_channels, conv1_features], stddev=0.1, dtype=tf.float32))
 conv1_bias = tf.Variable(tf.zeros([conv1_features], dtype=tf.float32))
 
@@ -41,6 +45,7 @@ conv2_weight = tf.Variable(tf.truncated_normal([4, 4, conv1_features, conv2_feat
 conv2_bias = tf.Variable(tf.zeros([conv2_features], dtype=tf.float32))
 
 # // means rounding down
+#Define Each Fully Layer's weight, bias
 resulting_width = image_width // (max_pool_size1 * max_pool_size2)
 resulting_height = image_height // (max_pool_size1 * max_pool_size2)
 full1_input_size = resulting_width * resulting_height * conv2_features
@@ -49,7 +54,7 @@ full1_bias = tf.Variable(tf.truncated_normal([fully_connected_size1], stddev=0.1
 full2_weight = tf.Variable(tf.truncated_normal([fully_connected_size1, target_size], stddev=0.1, dtype=tf.float32))
 full2_bias = tf.Variable(tf.truncated_normal([target_size], stddev=0.1, dtype=tf.float32))
 
-
+#Define ConvNet Conv1-relu1-Maxpooling1 -> Conv2-relu2-Maxpooling2 -> Flating Maxpooling2-Fully Layer-relu -> output
 def my_conv_net(input_data):
     # First Convolution - Relu - Maxpool Layer
     conv1 = tf.nn.conv2d(input_data, conv1_weight, strides=[1, 1, 1, 1], padding='SAME')
@@ -76,39 +81,44 @@ def my_conv_net(input_data):
 
 model_output = my_conv_net(x_input)
 test_model_output = my_conv_net(eval_input)
-
+#Define loss func
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=model_output, labels=y_target))
-
+#Use softmax func as the Last Layer
 prediction = tf.nn.softmax(model_output)
 test_prediction = tf.nn.softmax(test_model_output)
 
-
+#Define calculate accuracy
 def get_accuracy(logits, targets):
     batch_predictions = np.argmax(logits, axis=1)
     num_correct = np.sum(np.equal(batch_predictions, targets))
     return (100. * num_correct)/ batch_predictions.shape[0]
 
-
+#User Momentum Method that quit robust frequent change.to minimize loss func
 my_optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9)
 train_step = my_optimizer.minimize(loss)
-
+#Must init globals variables
 init = tf.global_variables_initializer()
 sess.run(init)
 
 train_loss = []
 train_acc = []
 test_acc = []
+#Train
 for i in range(generations):
+    #Get randomic indices for train
     rand_index = np.random.choice(len(train_xdata), size=batch_size)
+    #Get xdata, ydata for train
     rand_x = train_xdata[rand_index]
     rand_x = np.expand_dims(rand_x, 3)
     rand_y = train_labels[rand_index]
     train_dict = {x_input: rand_x, y_target: rand_y}
 
     sess.run(train_step, feed_dict=train_dict)
+    #get Temp train loss, acc
     temp_train_loss, temp_train_preds = sess.run([loss, prediction], feed_dict=train_dict)
     temp_train_acc = get_accuracy(temp_train_preds, rand_y)
 
+    #logging temp result
     if (i + 1) % eval_every == 0:
         eval_index = np.random.choice(len(test_xdata), size=evaluation_size)
         eval_x = test_xdata[eval_index]
@@ -127,3 +137,35 @@ for i in range(generations):
                         temp_test_acc]
         acc_and_loss = [np.round(x, 2) for x in acc_and_loss]
         print('Generation # {}. Train Loss: {:.2f}. Train Acc (Test Acc): {:.2f} ({:.2f})'.format(*acc_and_loss))
+
+eval_indices = range(0,generations, eval_every)
+#plotting Result
+plt.plot(eval_indices, train_loss, 'k-')
+plt.title('Softmax Loss per Generation')
+plt.xlabel('Generation')
+plt.ylabel('Softmax Loss')
+plt.show()
+
+plt.plot(eval_indices, train_acc, 'k-', label='Train Set Accuracy')
+plt.plot(eval_indices, test_acc, 'r--', label='Test Set Accuracy')
+plt.title('Train and Test Accuracy')
+plt.xlabel('Generation')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+
+#show result
+actuals = rand_y[0:6]
+predictions = np.argmax(temp_train_preds, axis =1)[0:6]
+images = np.squeeze(rand_x[0:6])
+
+Nrows = 2
+Ncols = 3
+for i in range(6):
+    plt.subplot(Nrows, Ncols, i+1)
+    plt.imshow(np.reshape(images[i], [28,28]),cmap='Greys_r')
+    plt.title('Actual: ' + str(actuals[i]) + ' Pred: ' + str(predictions[i]), fontsize=10)
+    frame=plt.gca()
+    frame.axes.get_xaxis().set_visible(False)
+    frame.axes.get_yaxis().set_visible(False)
+plt.show()
